cd250c40242b16af6e929529ef7b83ff1c27c644
Maria Rosolovska <feofanmaria19@gmail.com>
2022-01-24 14:10:35 +0200

    Updates

---
+++ precache-manifest.62b10e28004fb53a09945294ca052d75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9522394c085213d22f691b5912c53f6d",
+    "url": "/Timer-React/index.html"
+  },
+  {
+    "revision": "21bb25778fb549aa2b67",
+    "url": "/Timer-React/static/css/main.c17a1eaf.chunk.css"
+  },
+  {
+    "revision": "38b63845285aa863597e",
+    "url": "/Timer-React/static/js/2.b95c2202.chunk.js"
+  },
+  {
+    "revision": "3405545c67c2b04913629256d4fb5d95",
+    "url": "/Timer-React/static/js/2.b95c2202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21bb25778fb549aa2b67",
+    "url": "/Timer-React/static/js/main.3e750973.chunk.js"
+  },
+  {
+    "revision": "74b5522b455a21ad4e6b",
+    "url": "/Timer-React/static/js/runtime-main.0205de08.js"
+  }
+]);
